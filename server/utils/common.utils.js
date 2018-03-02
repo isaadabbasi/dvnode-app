012edbbf6bfe1ed2ea8fd7fdd70fdef317d34baa
Saad Abbasi <itssaadabbasi@gmail.com>
2018-03-03 01:56:19 +0500

    enhanced and documented common utils

--- server/utils/common.utils.js
+++ server/utils/common.utils.js
@@ -3,42 +3,57 @@ const
     { verifyJWT } = require('./promisify.utils'),
     { verifyToken } = require('./crypto.utils');
 
+/**
+ * @Author Saad Ababsi
+ * @class CommonUtils, commonUtils class for custom written or library modified methods.
+ */
+
 class CommonUtils {
+    /**
+     * @author Saad Abbasi
+     * @param {id} could be any string
+     * @returns boolean acc to if the param id is valid mongodb object id.
+     */
     isObjectId(id){
         return mongooseTypes.ObjectId.isValid(id);
     }
 
+    /**
+     * @author Saad Abbasi
+     * @description To be used as a middleware to check if JWT is available inside HTTP request.
+     */
+
     checkTokenInRequest(){
         return (request, response, next)=>{
-            const token = request.cookies.YAPSESSION;
+            const {USRSESSXX3: token = null} = request.cookies;
             if(token)
                 verifyToken(token)
                     .then(verified =>{
+                        // pass next to then in functional style.
                         next();
-                        console.log('is Token Verified: ', verified);
                     })
                     .catch(err => {
-                        if(err.message === 'jwt expired')
-                            response.status(403).send('Token/Session Expired');
-                        else 
-                            response.status(403).send('Oops! Invalid Session');
+                            response.status(403).send(err.message === 'jwt expired' ? 'Token/Session Expired': 'Oops! Invalid Session');
                     })
             else 
-                response.status(403).send('You must login');
+                response.status(403).send('Unauthorized user!');
         }
     }
 
-    commonCallback (resolve, reject, messages = {}){
-        let { errmsg = null, successmsg = null} = messages;   
+    /**
+     * @param {resolve} resolves the promise.
+     * @param {reject}  rejects the promise.
+     * @param {messages?}  (optional) to override retrieved values of body.
+     * @returns resolved/rejected promise object with two properties, {status, body} 
+     */
+    commonCallback (resolve, reject, { errmsg = null, successmsg = null }={}){
         return (err, body)=>{  
-            console.log(`common call back err: ${err}, message: ${body}`)
-            if(err) 
-                reject({ status: 500, body: errmsg || err.message }) 
-            if(!err) {
+            console.log(err, body);
+            err ? 
+                reject({ status: 500, body: errmsg || err.message }) : 
                 resolve({status: 200, body: successmsg || body });
-            }
         };
-    }            
+    }
 }
 
 module.exports = new CommonUtils();
